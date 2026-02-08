aabc53875d06cf4a0af520e607a44e9907724adb
yen.nguyen <yen.nguyen@digitaloutlook.com.au>
2026-02-08 16:29:05 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import InputTodo from './todo/input.todo'
 
@@ -7,9 +8,31 @@ function App() {
       age: 38
   }
 
+  const [listTodo, setListTodo] = useState([""]);
+  const [count, setCount] = useState<number>();
+  const handleCount = () => {   
+    if(count === undefined) {
+       setCount(0);     
+    } else {
+      setCount(count + 1); // count = count + 1;
+    }
+
+    console.log(count);
+  }
   return (
     <>
-      <InputTodo propsInfo={propsInfo}/>
+      <div>Count: {count}</div>
+      <br/>
+      <button onClick={handleCount}>Update Count</button>
+      <InputTodo propsInfo={propsInfo} listTodo={listTodo} setListTodo={setListTodo}/>
+      <br />
+      <ul>
+          { listTodo.map((item, index) => {
+              return(
+                  <li key={index}>{item}</li>
+              );
+          })}          
+      </ul>
     </>
   )
 }
--- src/todo/input.todo.tsx
+++ src/todo/input.todo.tsx
@@ -1,30 +1,48 @@
+import { useState } from "react";
+
 interface IProps {
     propsInfo: {
         name: string,
         age: number
-    }
+    },
+    listTodo: string[],
+    setListTodo: (v: string[]) => void
 }
 
 const InputTodo = (props: IProps) => {
     
-    const {propsInfo} = props;
+    const {propsInfo, listTodo, setListTodo} = props;
+
+    // todo là state để manage status của input value
+    const [todo, setTodo] = useState("");
+
+    // Không gọi setTodo() trực tiếp trong body component → gây vòng lặp re-render vô hạn.
+    // Chỉ gọi setTodo trong event handler (onChange, onClick) hoặc trong useEffect.    
+
+    const handleClick = () => {
+        if(!todo) { //todo = "" = false;        
+            alert("Please enter value item");
+            return;
+        }
+        setListTodo([...listTodo, todo]);
+        setTodo(""); // set todo = "" sau khi đã update listTodo
+    }
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTodo(e.target.value);
+    }
 
     return (
-        <div style={{ border: "1px solid red" }}>
-            <div>Get props: {JSON.stringify(propsInfo)}</div>
+        <div>
+            {/* <div>Get props: {JSON.stringify(propsInfo)}</div> */}
             <div>Add new todo</div>
             <input
-                value="value item"
-                type="text"            
+                value={todo} // khởi tạo value ""
+                type="text"
+                onChange={handleOnChange}      
             />
             &nbsp; &nbsp;
-            <button>Save</button>
-            <br />
-            <ul>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>            
-            </ul>
+            <button onClick={handleClick}>Save</button>            
         </div>
     )
 }
@@ -36,4 +54,7 @@ export default InputTodo;
  * 2. create Html for form input todo  
  * 3. create variables from parent component, sau đó truyền xuống child component thông qua props
  * 4. define data type on child component để get props data từ parent
+ * 5. create handleOnChange để get value assign cho todo thông qua setTodo
+ * 6. create handleClick, add todo to listTodo
+ * 7. hiển thị item bằng listTodo.map()
  */
