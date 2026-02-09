ee7b7ade44160ea08afded89fbc45d8fd5cf593e
yennguyen10 <yen.nguyen@digitaloutlook.com.au>
2026-02-09 14:41:40 +0700

    update

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,31 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../../styles/users.css';
+
+interface IUsers {
+    _id: string,
+    email: string,
+    name: string,
+    address: string
+}
 
 const UsersTable = () => {
 
-    const getData = async () => {
+    const [listUsers, setListUsers] = useState([]);
+
+
+    const getData = async () => {       
+
         // api url, method, body, headers
-        const res = await fetch(            
-            "http://localhost:8000/api/v1/auth/login",
-            {
-                method: "POST",
-                body: JSON.stringify({
-                    username: "hoidanit@gmail.com",
-                    password: "123456"
-                }),
-                headers: {
-                    "Content-type": "Aplication/json"
-                }
-            }
-        );
+        // const res = await fetch(            
+        //     "http://localhost:8000/api/v1/auth/login",
+        //     {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify({
+        //             username: "hoidanit@gmail.com",
+        //             password: "123456"
+        //         })
+        //     }
+        // );
 
         //res.json()
         // const data = await res.json();
 
-        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjk1MGFlZTQ1NmE3MzFkNjQwMDI2MmVkIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3NzA1NDUxMDYsImV4cCI6MTg1Njk0NTEwNn0.AuWh5Kv5XBCED-XH54yCR8Bbb9BORWf6DqcVp9LneBk";
+        // console.log("data:", data);       
 
+        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjk1MGFlZTQ1NmE3MzFkNjQwMDI2MmVkIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3NzA2MDc2NjMsImV4cCI6MTg1NzAwNzY2M30.pzfUkOUBzdTI7G8AIRr6DsSr7oDqyg-B684VA9M7PBM";
         const fetchUsers = await fetch(
-            "http://localhost:8000/api/v1/users/all",
+            "http://localhost:8000/api/v1/users/all", 
             {
                 headers: {
                     "Authorization": `Bearer ${access_token}`,
@@ -36,7 +48,9 @@ const UsersTable = () => {
 
         const dataUsers = await fetchUsers.json();
 
-        console.log("dataUsers:", dataUsers);
+        // add user data to listUsers
+        setListUsers(dataUsers.data.result);
+        // console.log("dataUsers:", dataUsers);
     }
 
     useEffect(()=>{
@@ -46,46 +60,23 @@ const UsersTable = () => {
     return(
         <div>
             <h2>HTML Table</h2>
-
             <table>
                 <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                </tr>    
+            
+                { listUsers.map((item: IUsers) => {
+                    return (
+                        <tr key={item._id}>
+                            <td>{item.email}</td>
+                            <td>{item.name}</td>
+                            <td>{item.address}</td>
+                        </tr>    
+                    );
+                }) }                            
             </table>
-
-
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,62 @@
-import React, { StrictMode } from 'react'
+import React, { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet,
+  Link
 } from "react-router-dom"
 // import './index.css'
 import App from './App.tsx'
 import UsersPage from './screens/users.page.tsx'
 
+import { FireOutlined, TeamOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to={'/'}>Home</Link>,
+    key: 'home',
+    icon: <FireOutlined />,
+  },
+  {
+    label: <Link to={'/users'}>Manage Users</Link>,
+    key: 'users',
+    icon: <TeamOutlined />,
+  }
+];
+
+const Header = () => {
+  const [current, setCurrent] = useState('mail');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+}
+
+const GeneralLayout = () => {
+  return(
+    <>
+    <Header/>
+    <Outlet/>
+    </>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/users",
-    element: <UsersPage/>,
+    element: <GeneralLayout/>,
+    children: [
+      { index: true, element: <App /> },
+      { path: "users", element: <UsersPage/> }
+    ]
   },
   {
     path: "/tracks",
